c1730e91f6b33ad4c21fd7d28b680c6933dc5d62
247698 Kołata Mateusz <247698@edu.p.lodz.pl>
2024-03-24 17:03:09 +0100

    algorytm fix

--- src/main/java/com/example/algorytm3des/TripleDES.java
+++ src/main/java/com/example/algorytm3des/TripleDES.java
@@ -3,107 +3,63 @@ package com.example.algorytm3des;
 import java.util.Arrays;
 
 public class TripleDES extends DES {
-    public byte[] TripleDES_Encrypt(byte[] data, byte[][] keys)
-    {
-        int lenght;
-        byte[] padding;
-        int i;
-
-        lenght = 8 - data.length % 8;
-        padding = new byte[lenght];
-        padding[0] = (byte) 0x80;
-
-        for (i = 1; i < lenght; i++)
-            padding[i] = 0;
-
-
-        byte[] tmp = new byte[data.length + lenght];
-        byte[] bloc = new byte[8];
-
+    public byte[] TripleDES_Encrypt(byte[] data, byte[][] keys) {
+        byte[] paddedData = addPadding(data);
+        byte[] encryptedData = new byte[paddedData.length];
 
         subKeys1 = generateSubKeys(keys[0]);
         subKeys2 = generateSubKeys(keys[1]);
         subKeys3 = generateSubKeys(keys[2]);
 
-        int count = 0;
+        for (int i = 0; i < paddedData.length; i += 8) {
+            byte[] block = Arrays.copyOfRange(paddedData, i, i + 8);
+            byte[] encryptedBlock = encryptBlock(block);
+            System.arraycopy(encryptedBlock, 0, encryptedData, i, 8);
+        }
 
-        for (i = 0; i < data.length + lenght; i++) {
-            if (i > 0 && i % 8 == 0) {
-                bloc = encrypt64Bloc(bloc, subKeys1, true);
-                bloc = encrypt64Bloc(bloc, subKeys2, false);
-                bloc = encrypt64Bloc(bloc, subKeys3, true);
-                System.arraycopy(bloc, 0, tmp, i - 8, bloc.length);
-            }
-            if (i < data.length)
-                bloc[i % 8] = data[i];
-            else {
-                bloc[i % 8] = padding[count % 8];
-                count++;
-            }
-        }
-        if(bloc.length == 8){
-            bloc = encrypt64Bloc(bloc, subKeys1, true);
-            bloc = encrypt64Bloc(bloc, subKeys2, false);
-            bloc = encrypt64Bloc(bloc, subKeys3, true);
-            System.arraycopy(bloc, 0, tmp, i - 8, bloc.length);
-        }
-        return tmp;
+        return encryptedData;
     }
 
-    public byte[] TripleDES_Decrypt(byte[] data, byte[][] keys)
-    {
-        int i;
-        byte[] tmp = new byte[data.length];
-        byte[] bloc = new byte[8];
+    public byte[] TripleDES_Decrypt(byte[] data, byte[][] keys) {
+        byte[] decryptedData = new byte[data.length];
 
         subKeys1 = generateSubKeys(keys[0]);
         subKeys2 = generateSubKeys(keys[1]);
         subKeys3 = generateSubKeys(keys[2]);
 
-        for (i = 0; i < data.length; i++) {
-            if (i > 0 && i % 8 == 0) {
-                bloc = encrypt64Bloc(bloc, subKeys3, false);
-                bloc = encrypt64Bloc(bloc, subKeys2, true);
-                bloc = encrypt64Bloc(bloc, subKeys1, false);
-                System.arraycopy(bloc, 0, tmp, i - 8, bloc.length);
-            }
-            bloc[i % 8] = data[i];
+        for (int i = 0; i < data.length; i += 8) {
+            byte[] block = Arrays.copyOfRange(data, i, i + 8);
+            byte[] decryptedBlock = decryptBlock(block);
+            System.arraycopy(decryptedBlock, 0, decryptedData, i, 8);
         }
-        bloc = encrypt64Bloc(bloc, subKeys3, false);
-        bloc = encrypt64Bloc(bloc, subKeys2, true);
-        bloc = encrypt64Bloc(bloc, subKeys1, false);
-        System.arraycopy(bloc, 0, tmp, i - 8, bloc.length);
 
-
-        tmp = deletePadding(tmp);
-
-        return tmp;
+        return removePadding(decryptedData);
+    }
+    byte[] encryptBlock(byte[] block) {
+        byte[] result = Arrays.copyOf(block, block.length);
+        result = encrypt64Bloc(result, subKeys1, true);
+        result = encrypt64Bloc(result, subKeys2, false);
+        result = encrypt64Bloc(result, subKeys3, true);
+        return result;
     }
 
-    public static byte[] addPadding(byte[] data) {
+    byte[] decryptBlock(byte[] block) {
+        byte[] result = Arrays.copyOf(block, block.length);
+        result = encrypt64Bloc(result, subKeys3, false);
+        result = encrypt64Bloc(result, subKeys2, true);
+        result = encrypt64Bloc(result, subKeys1, false);
+        return result;
+    }
+
+    byte[] addPadding(byte[] data) {
         int paddingLength = 8 - (data.length % 8);
         byte[] paddedData = Arrays.copyOf(data, data.length + paddingLength);
         Arrays.fill(paddedData, data.length, paddedData.length, (byte) paddingLength);
         return paddedData;
     }
 
-    // Usuwanie paddingu z tekstu
-    public static byte[] removePadding(byte[] data) {
+    byte[] removePadding(byte[] data) {
         int paddingLength = data[data.length - 1];
         return Arrays.copyOf(data, data.length - paddingLength);
-    }
-
-    byte[] deletePadding(byte[] input) {
-        int count = 0;
-
-        int i = input.length - 1;
-        while (input[i] == 0) {
-            count++;
-            i--;
-        }
-
-        byte[] tmp = new byte[input.length - count - 1];
-        System.arraycopy(input, 0, tmp, 0, tmp.length);
-        return tmp;
     }
 }
