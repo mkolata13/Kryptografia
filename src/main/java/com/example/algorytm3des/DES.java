fe056f61bf4a68f2eb9b11c7405ba15e703a5417
247698 Kołata Mateusz <247698@edu.p.lodz.pl>
2024-03-25 18:41:07 +0100

    poprawki v.1.1

--- src/main/java/com/example/algorytm3des/DES.java
+++ src/main/java/com/example/algorytm3des/DES.java
@@ -52,7 +52,7 @@ public class DES {
             }
     };
 
-    // Permutation P (Feistel function)
+    // permutacja P w funkcji feistela
     private int[] P = { 16,  7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26,  5, 18, 31, 10,
                          2,  8, 24, 14, 32, 27,  3,  9, 19, 13, 30,  6, 22, 11,  4, 25
     };
